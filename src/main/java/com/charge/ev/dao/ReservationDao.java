3fc67f0a535f425731568e5a44cfe95ac9a581a6
balajichittala <balajichittala17@gmail.com>
2025-02-02 12:43:14 +0530

    bookingstatus and view slot

--- src/main/java/com/charge/ev/dao/ReservationDao.java
+++ src/main/java/com/charge/ev/dao/ReservationDao.java
@@ -3,8 +3,10 @@ package com.charge.ev.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.charge.ev.entries.Reservation;
 
@@ -16,4 +18,10 @@ public interface ReservationDao extends JpaRepository<Reservation, Long>{
 
 	public List<Reservation> findByUserId(String userId);
 	public List<Reservation> findByVendorid(String vendorid);
+	public List<Reservation> findByStationID(long stationid);
+	
+	 @Transactional
+	 @Modifying
+	 @Query(value="update slot_booking set status=?1 where rid=?2" , nativeQuery = true)
+	 public void updateStatus(String status,long rid);
 }
